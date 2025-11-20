c918a310a7485b1a7e4e606efeea4c746b7ed041
Munyeshya <obedbenny54@gmail.com>
2025-11-21 01:52:18 +0200

    Wrap the app with AuthProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { AuthProvider } from "../contexts/AuthContext";
+
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
@@ -10,11 +12,13 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <QueryClientProvider client={queryClient}>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-            }}
-          />
+          <AuthProvider>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+              }}
+            />
+          </AuthProvider>
         </QueryClientProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
