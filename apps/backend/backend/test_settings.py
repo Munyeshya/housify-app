cbc874c55a838cf032d5d8723fae46cb986d04a2
Munyeshya <obedbenny54@gmail.com>
2026-04-06 01:20:19 +0200

    Remove temporary SQLite test settings

--- apps/backend/backend/test_settings.py
+++
@@ -1,9 +0,0 @@
-from .settings import *  # noqa: F403,F401
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "test_db.sqlite3",  # noqa: F405
-    }
-}
