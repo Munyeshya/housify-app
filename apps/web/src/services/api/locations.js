efc636b31fbb981d9cbfc2fd3df1bd729d8311ec
Munyeshya <obedbenny54@gmail.com>
2026-04-09 01:37:35 +0200

    Add web API service for map hierarchy counts

---
+++ apps/web/src/services/api/locations.js
@@ -0,0 +1,32 @@
+import { apiClient } from "./client"
+import { apiEndpoints } from "./endpoints"
+
+const withQuery = (endpoint, query = {}) => {
+  const searchParams = new URLSearchParams()
+
+  Object.entries(query).forEach(([key, value]) => {
+    if (value === undefined || value === null || value === "") {
+      return
+    }
+
+    searchParams.set(key, String(value))
+  })
+
+  const queryString = searchParams.toString()
+  return queryString ? `${endpoint}?${queryString}` : endpoint
+}
+
+export const locationsApi = {
+  listDistrictCounts(query = {}) {
+    return apiClient.get(withQuery(apiEndpoints.locations.districtCounts, query))
+  },
+  listSectorCounts(districtId, query = {}) {
+    return apiClient.get(withQuery(apiEndpoints.locations.sectorCounts(districtId), query))
+  },
+  listCellCounts(sectorId, query = {}) {
+    return apiClient.get(withQuery(apiEndpoints.locations.cellCounts(sectorId), query))
+  },
+  listVillageCounts(cellId, query = {}) {
+    return apiClient.get(withQuery(apiEndpoints.locations.villageCounts(cellId), query))
+  },
+}
