f857ad35591d53b223dd4e472e46174a0bca1e50
Munyeshya <obedbenny54@gmail.com>
2026-04-08 17:21:23 +0200

    Turn legacy app layout into dashboard layout wrapper

--- apps/web/src/layouts/AppLayout.jsx
+++ apps/web/src/layouts/AppLayout.jsx
@@ -1,21 +1,3 @@
-import { Outlet } from "react-router-dom"
-import AppSidebar from "../components/AppSidebar"
-import DashboardHeader from "../components/DashboardHeader"
+import DashboardLayout from "./DashboardLayout"
 
-function AppLayout() {
-  return (
-    <div className="app-shell">
-      <DashboardHeader />
-      <div className="app-shell__body">
-        <AppSidebar />
-        <main className="app-shell__content">
-          <div className="container-fluid">
-            <Outlet />
-          </div>
-        </main>
-      </div>
-    </div>
-  )
-}
-
-export default AppLayout
+export default DashboardLayout
