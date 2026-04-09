9e5ddc937de7c4b1687533097784ddd38d6c6e2a
Munyeshya <obedbenny54@gmail.com>
2026-04-09 20:04:00 +0200

    Align dashboard shell with Housify theme

--- apps/web/src/layouts/DashboardLayout.jsx
+++ apps/web/src/layouts/DashboardLayout.jsx
@@ -5,14 +5,16 @@ import DashboardSidebar from "../components/layout/DashboardSidebar"
 function DashboardLayout() {
   return (
     <div className="app-shell">
-      <DashboardHeader />
       <div className="app-shell__body">
         <DashboardSidebar />
-        <main className="app-shell__content">
-          <div className="container-fluid">
-            <Outlet />
-          </div>
-        </main>
+        <div className="app-shell__workspace">
+          <DashboardHeader />
+          <main className="app-shell__content">
+            <div className="container-fluid">
+              <Outlet />
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   )
